037c352c6e0a8cbc5d36e93c668aa0b4b34577bf
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-02-09 06:20:50 +0200

    feature: login layout

--- App.js
+++ App.js
@@ -1,29 +1,102 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {StatusBar} from 'expo-status-bar';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 import {useEffect} from "react";
 import * as http from "./util/http";
+import {Colors} from "./constants/colors";
 
 export default function App() {
-  async function getData() {
-    return await http.fetchData()
-  }
+    async function getData() {
+        return await http.fetchData()
+    }
 
-  useEffect(() => {
-    getData().then((d)=>console.log(d.data))
-  })
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    // useEffect(() => {
+    //   getData().then((d)=>console.log(d.data))
+    // })
+    return (
+        <View style={styles.container}>
+            <View style={styles.username_container}>
+                <View>
+                    <Text style={styles.username_text}>Username</Text>
+                </View>
+                <View>
+                    <TextInput style={styles.username}/>
+                </View>
+            </View>
+
+            <View style={styles.password_container}>
+                <View>
+                    <Text style={styles.password_text}>Password</Text>
+                </View>
+                <View>
+                    <TextInput secureTextEntry={true} style={styles.password}/>
+                </View>
+            </View>
+
+            <View style={styles.button_container}>
+                <View style={styles.login_button}>
+                    <Button color={Colors.light_dark} title={"Login"}/>
+                </View>
+                <View style={styles.register_button}>
+                    <Button color={Colors.light} title={"Register"}/>
+                </View>
+            </View>
+            <StatusBar style="light"/>
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: Colors.dark,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    username_container: {
+        width: '80%',
+    },
+    username_text: {
+        color: Colors.light,
+        fontWeight: "bold"
+    },
+    username: {
+        fontSize: 20,
+        borderColor: Colors.hard_dark,
+        color: Colors.light,
+        paddingHorizontal: 5,
+        borderWidth: 2,
+        width: '100%',
+        borderRadius: 5,
+        marginBottom: 5,
+    },
+    password_container: {
+        width: '80%',
+    },
+    password_text: {
+        color: Colors.light,
+        fontWeight: "bold"
+    },
+    password: {
+        fontSize: 20,
+        borderColor: Colors.hard_dark,
+        borderWidth: 2,
+        width: '100%',
+        borderRadius: 5,
+        color: Colors.light,
+        paddingHorizontal: 5,
+    },
+    button_container: {
+        width: "80%",
+        marginTop: 15,
+        flexDirection: "row"
+    },
+    login_button: {
+        width: "48%",
+        marginHorizontal: 2
+    },
+    register_button: {
+        width: "48%",
+        marginHorizontal: 2,
+        color: Colors.light
+    }
 });
---
+++ constants/colors.js
@@ -0,0 +1,8 @@
+// https://colorhunt.co/palette/0306373c0753720455910a67
+export const Colors = {
+    dark: "#030637",
+    hard_dark: "#3C0753",
+    light_dark: "#720455",
+    light: "#910A67",
+};
+
