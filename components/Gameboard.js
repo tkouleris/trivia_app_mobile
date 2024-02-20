68f246565907399c7efde8e50745c514b83cbf0f
Thodoris Kouleris <kurokokoro@windowslive.com>
2024-02-20 06:20:05 +0200

    feature: next question button

--- components/Gameboard.js
+++ components/Gameboard.js
@@ -1,33 +1,50 @@
-import {View, Text, StyleSheet} from "react-native";
+import {View, Text, StyleSheet, Pressable} from "react-native";
 import {useEffect, useLayoutEffect, useState} from "react";
 import {Colors} from "../constants/colors";
+import LoadingOverlay from "./UI/LoadingOverlay";
 
 function Gameboard({route}){
     const [questions, setQuestions] = useState([])
     const [currentIndex, setCurrentIndex] = useState(0)
+    const [totalPoints, setTotalPoints] = useState(0)
 
     useEffect(() => {
         setQuestions(route.params.questions)
-
     }, []);
 
     function decode(string){
-        return decodeURI(string).replace("%3F","?")
+        console.log(string)
+        let output = decodeURI(string).replace("%3F","?");
+        output = output.replace("%3A", ":")
+        return output.replace("%2C",",")
     }
 
+    function answerHandler(points){
+        if(currentIndex < questions.length){
+            setCurrentIndex(currentIndex+1)
+            setTotalPoints(totalPoints + points)
+        }
+        console.log("Total Points: " + totalPoints);
+    }
 
     if(questions.length === 0){
-        return <View>
-            <Text>Gameboard</Text>
-        </View>
-    }else{
-        return <View style={styles.container}>
-            <Text style={styles.question_text}>{(currentIndex+1)}. { decode(questions[currentIndex].question)}</Text>
-        </View>
+        return <LoadingOverlay/>
     }
 
-
-
+    return <View style={styles.container}>
+        <Text style={styles.question_text}>{(currentIndex+1)}. { decode(questions[currentIndex].question)}</Text>
+        <View style={styles.answers_container}>
+            {questions[currentIndex].answers.map((q, key) => {
+                return (
+                    <Pressable onPress={answerHandler.bind(this,q.points)} style={styles.answer_container} key={key}>
+                        <View >
+                            <Text style={styles.answer_text}>{decode(q.answer)}</Text>
+                        </View>
+                    </Pressable>
+                );
+            })}
+        </View>
+    </View>
 }
 
 export default Gameboard;
@@ -45,5 +62,23 @@ const styles = StyleSheet.create({
         color: Colors.light,
         fontWeight: 'bold',
         fontSize: 22
+    },
+    answers_container:{
+        width: '100%',
+        marginTop: 20
+    },
+    answer_container:{
+        paddingVertical: 5,
+        borderWidth: 1,
+        borderColor: Colors.hard_dark,
+        width: '100%',
+        marginVertical: 10
+    },
+    answer_text:{
+        textAlign: 'left',
+        color: Colors.light,
+        fontWeight: 'bold',
+        paddingLeft: 10,
+        fontSize: 20
     }
 })
